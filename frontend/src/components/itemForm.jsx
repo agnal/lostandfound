d2cf7949430ec61832146166de4d3ff843d1c946
agnal <agnal.george001@gmail.com>
2025-08-20 16:44:20 +1000

    Pipleine001

--- frontend/src/components/itemForm.jsx
+++ frontend/src/components/itemForm.jsx
@@ -2,22 +2,22 @@ import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import axiosInstance from '../axiosConfig';
 import { useNavigate } from 'react-router-dom';
+
 const ItemForm = ({ item }) => {
   const { user } = useAuth();
-  const [formData, setFormData] = useState({ title: '', description: '', });
+  const [formData, setFormData] = useState({ title: '', description: '' });
   const [image, setImage] = useState(null);
   const navigate = useNavigate();
-  useEffect(() => {
 
+  useEffect(() => {
     if (item) {
-      
       setFormData({
         title: item.title,
         description: item.description,
         deadline: item.deadline,
       });
     } else {
-      setFormData({ title: '', description: '', });
+      setFormData({ title: '', description: '' });
     }
   }, [item]);
 
@@ -25,56 +25,44 @@ const ItemForm = ({ item }) => {
     e.preventDefault();
 
     try {
-      // Build multipart form
-    
-      // if (image) data.append("image", image);
-
-      let response;
       if (item) {
-          const data = {}
-      data["title"]= formData.title
-      data["description"]= formData.description;
-        response = await axiosInstance.put(
-          `/api/main/item/${item._id}`,
-          data,
-          {
-            headers: {
-              Authorization: `Bearer ${user.token}`,
-            
-            },
-          }
-        );
+        const data = {
+          title: formData.title,
+          description: formData.description,
+        };
 
-      } else {
-        const data = new FormData();
-        data.append("title", formData.title);
-        data.append("description", formData.description);
-        if (image) data.append("image", image);
-        response = await axiosInstance.post("/api/main/item", data, {
+        await axiosInstance.put(`/api/main/item/${item._id}`, data, {
           headers: {
             Authorization: `Bearer ${user.token}`,
-            "Content-Type": "multipart/form-data",
           },
         });
+      } else {
+        const data = new FormData();
+        data.append('title', formData.title);
+        data.append('description', formData.description);
+        if (image) data.append('image', image);
 
+        await axiosInstance.post('/api/main/item', data, {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        });
       }
 
-
-      setFormData({ title: "", description: "", deadline: "" });
+      setFormData({ title: '', description: '', deadline: '' });
       setImage(null);
-      navigate('/')
-
+      navigate('/');
     } catch (error) {
       console.log(error);
-
-      alert("Failed to save task.");
+      alert('Failed to save task.');
     }
   };
-
 
   return (
     <form onSubmit={handleSubmit} className="bg-white p-6 shadow-md rounded mb-6">
       <h1 className="text-2xl font-bold mb-4">{item ? 'Edit Item' : 'Add Items'}</h1>
+
       <input
         type="text"
         placeholder="Item name"
@@ -82,6 +70,7 @@ const ItemForm = ({ item }) => {
         onChange={(e) => setFormData({ ...formData, title: e.target.value })}
         className="w-full mb-4 p-2 border rounded"
       />
+
       <input
         type="text"
         placeholder="Description"
@@ -89,7 +78,6 @@ const ItemForm = ({ item }) => {
         onChange={(e) => setFormData({ ...formData, description: e.target.value })}
         className="w-full mb-4 p-2 border rounded"
       />
-
 
       {!item && (
         <input
@@ -99,6 +87,7 @@ const ItemForm = ({ item }) => {
           className="w-full mb-4 p-2 border rounded"
         />
       )}
+
       <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded">
         {item ? 'Update Item' : 'Create Item'}
       </button>
