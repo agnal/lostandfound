97b45afa731455d015aee387797688f3eacea605
agnal <agnal.george001@gmail.com>
2025-08-20 13:53:09 +1000

    Pipleine 2

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.27.215.25:5001', // live
+  //baseURL: '3.107.69.235:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
