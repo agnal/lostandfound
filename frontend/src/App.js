ef2392e1320102673b6def964237e5840550180f
steve <stevesoneyvarghese13@gmail.com>
2025-10-02 16:42:53 +1000

    Cleared error in Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import HomePage from './pages/Tasks';
 import AddItems from './pages/add_items';
 
 
@@ -15,7 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/" element={<Tasks />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/add-items" element={<AddItems />} />
 
       </Routes>
